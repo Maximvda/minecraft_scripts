114e0a1abf4016817d20224b0268685c98dfd48e
siemie <maximvda123@gmail.com>
2023-05-26 00:40:05 +0200

    Progress towards mining class but got to sleep now

--- computer/1/main.lua
+++ computer/1/main.lua
@@ -1,29 +1,30 @@
 -- Main turtle code which mines and also periodically sends statistics
 os.loadAPI("components/link")
 os.loadAPI("components/config")
-os.loadAPI("components/miner")
+os.loadAPI("components/mine_api")
 os.loadAPI("components/turtle")
 
+-- Initialisation
+miner = mine_api.init()
+link.init(config.modem_side)
 update_timer = os.startTimer(5)
 
-info = {}
 
-local function main_loop()
+local function event_loop()
     while true do
-        event, par1, par2, par3 = os.pullEvent()
-        if event == "rednet_message" then
+        event = {os.pullEvent()}
+        if event[1] == "rednet_message" then
             link.handle_message(par1, par2)
         end
-        if event == "key" and par1 == 57 then
+        if event[1] == "key" and event[2] == 57 then
             return
         end
 
-        if event == "timer" and par1 == update_timer then
+        if event[1] == "timer" and event[2] == update_timer then
             miner.get_info(info)
             update_timer = os.startTimer(5)
             link.send_data(info)
         end
-
     end
 end
 
@@ -33,11 +34,10 @@ local function turtle_loop()
     end
 end
 
--- Initialise stuff
-link.init(config.modem_side)
+print(miner.falling_block())
 
 -- Run the os loop and turtle tick simulatinous
 parallel.waitForAny(
     turtle_loop,
-    main_loop
+    event_loop
 )
