ccf6af3afe4da38ea445d101375dc27e7abe21f4
siemie <maximvda123@gmail.com>
2023-06-13 19:11:55 +0200

    update clone script

--- get_git_files.lua
+++ get_git_files.lua
@@ -1,8 +1,10 @@
-
-
-
 local function get(pc, script_name, folder)
-    local download = http.get("https://raw.githubusercontent.com/Maximvda/minecraft_scripts/main/computer/"..pc..folder..script_name) --This will make 'download' hold the contents of the file.
+    if folder == "" then
+        file_name = pc.."/"..script_name
+    else
+        file_name = pc.."/"..folder.."/"..script_name
+    end
+    local download = http.get("https://raw.githubusercontent.com/Maximvda/minecraft_scripts/main/computer/"..file_name) --This will make 'download' hold the contents of the file.
     if download then --checks if download returned true or false
         local handle = download.readAll() --Reads everything in download
         download.close() --remember to close the download!
@@ -16,14 +18,14 @@ local function get(pc, script_name, folder)
         file.write(handle) --writes all the stuff in handle to the file defined in 'saveTo'
         file.close() --remember to close the file!
     else --if returned false
-        print("Unable to download the file "..file)
+        print("Unable to download the file "..pc)
         print("Make sure you have the HTTP API enabled or")
         print("an internet connection!")
     end --end the if
 end --close the function
 
 local function clone_pc(pc)
-    if pc == 0 then
+    if pc == "0" then
         get("0", "main.lua", "")
         get("0", "button_class", "components")
         get("0", "config", "components")
@@ -32,3 +34,8 @@ local function clone_pc(pc)
         get("0", "link", "components")
     end
 end
+
+print("Which pc to clone?")
+pc = read()
+print("Cloning: "..pc)
+clone_pc(pc)
