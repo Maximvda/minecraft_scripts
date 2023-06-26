b42f64ed329ab25d1ed2e4fc9627444d03dbc7be
siemie <maximvda123@gmail.com>
2023-06-27 00:29:23 +0200

    Updated turtle script to command chunky

--- computer/4/startup.lua
+++ computer/4/startup.lua
@@ -1,12 +1,10 @@
 -- Main turtle code which mines and also periodically sends statistics
 os.loadAPI("components/link_class")
 os.loadAPI("components/config")
-os.loadAPI("components/mine_api")
---os.loadAPI("components/logger")
---os.loadAPI("components/turtle")
+os.loadAPI("components/chunky")
 
 -- Initialisation
-miner = mine_api.init()
+chunky = chunky.init()
 
 local function callback_message(message)
     if message == "Start" then
@@ -22,8 +20,6 @@ end
 
 
 local link = link_class.init(config.modem_side, callback_message)
-update_timer = os.startTimer(10)
-
 
 local function event_loop()
     while true do
@@ -34,23 +30,10 @@ local function event_loop()
         if event[1] == "key" and event[2] == 57 then
             return
         end
-
-        if event[1] == "timer" and event[2] == update_timer then
-            info = miner.get_info(info)
-            update_timer = os.startTimer(10)
-            link.send_data(info)
-        end
-    end
-end
-
-local function turtle_loop()
-    while true do
-        miner.tick()
     end
 end
 
 -- Run the os loop and turtle tick simulatinous
 parallel.waitForAny(
-    turtle_loop,
     event_loop
 )
